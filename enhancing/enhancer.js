5e13c8588cb985ae78e55ecf6d31763c41db5640
Issac Moreno <issaxavier909@gmail.com>
2020-03-25 15:54:30 -0700

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,59 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const newItem = {
+      name: item.name,
+      durability: 100,
+      enhancement: item.enhancement + 1
+    };
+    return newItem;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const lowLevel = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+
+    return lowLevel;
+
+  } else if (item.enhancement === 15) {
+    const midLevel = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement
+    };
+
+    return midLevel;
+
+} else if (item.enhancement > 15) {
+    const highLevel = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    };
+
+    return highLevel;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,97 @@
-const enhancer = require('./enhancer.js');
+const { succeed, repair, fail } = require("./enhancer.js");
+
+describe("enhancer.js", () => {
+  describe(".succeed()", () => {
+    it("should level up enhancement by 1", () => {
+      const item = {
+        name: "Saradomin Sword",
+        durability: 21,
+        enhancement: 19
+      };
+      const expected = {
+        name: "Saradomin Sword",
+        durability: 100,
+        enhancement: 20
+      };
+
+      const enhancedItem = succeed(item);
+      expect(enhancedItem).toEqual(expected);
+    });
+  });
+
+  describe(".repair()", () => {
+    it("takes in an item and changes durability to 100", () => {
+      const item = {
+        name: "Saradomin Sword",
+        durability: 10,
+        enhancement: 15
+      };
+
+      const expected = {
+        name: "Saradomin Sword",
+        durability: 100,
+        enhancement: 15
+      };
+
+      const repairedItem = repair(item);
+
+      expect(repairedItem).toEqual(expected);
+    });
+  });
+
+  describe(".fail", () => {
+    it("decreases durability by 5 below level 15 enhancement", () => {
+      const lowLevelItem = {
+        name: "Saradomin Sword",
+        durability: 50,
+        enhancement: 14
+      };
+
+      const lowLevelExpected = {
+        name: "Saradomin Sword",
+        durability: 45,
+        enhancement: 14
+      };
+
+      const failLowLevel= fail(lowLevelItem);
+
+      expect(failLowLevel).toEqual(lowLevelExpected);
+    });
+
+    it("decreases durability by 10 above level 15 enhancement", () => {
+        const midLevelItem = {
+            name: "Saradomin Sword",
+            durability: 33,
+            enhancement: 15
+        };
+
+        const midLevelExpected = {
+            name: "Saradomin Sword",
+            durability: 23,
+            enhancement: 15
+        };
+
+        const failMidLevel = fail(midLevelItem);
+
+        expect(failMidLevel).toEqual(midLevelExpected);
+    });
+
+    it("decreases the durability by 10 and enhancement by 1 for items with enhancement above level 15", () => {
+        const highLevelItem = {
+            name: "Saradomin Sword",
+            durability: 23,
+            enhancement: 29
+        };
+
+        const highLevelExpected = {
+            name: "Saradomin Sword",
+            durability: 13,
+            enhancement: 28
+        };
+
+        const failHighLevel = fail(highLevelItem)
+        expect(failHighLevel).toEqual(highLevelExpected)
+    })
+  });
+});
 // test away!
